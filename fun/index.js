3329faaecc75d6c3d7d1267b31d07e3205f88bdc
thevjay <rebalvijay930@gmail.com>
2025-10-12 18:12:34 +0530

    first commit

---
+++ fun/index.js
@@ -0,0 +1,6 @@
+function printHelloWorld(){
+    console.log("Hello world")
+}
+
+let a = 10
+printHelloWorld()
---
+++ programming101/index.js
@@ -0,0 +1,45 @@
+console.log("Hello World!")
+
+7
+"7"
+Boolean => true, false
+
+const a = 10
+const b = 20
+
+const sum = a + b;
+
+let x = 20;
+
+console.log(x);
+
+x = 30;
+
+console.log(x);
+
+let firstName = "firstName";
+let lastName = 7;
+
+firstName = "7"
+
+let fullName = firstName +" "+lastName;
+
+console.log(fullName)
+
+let arr = [2,6,8,100,9,27]
+
+let sum1 = arr[0] + arr[4]
+
+console.log(arr[4])
+console.log(sum1)
+
+let arr1 = ["vik","nk","wfe"];
+
+let arr3 = ["k","nkjkj",3,true,"xys"];
+
+let obj = {
+    a:7,
+    name: "vjm",
+    bool: true,
+    
+}
