99a0ba40b22dd28dd5a6461199989d5caf4ad73f
thevjay <rebalvijay930@gmail.com>
2025-10-13 08:59:56 +0530

    patterns

---
+++ loops/patterns.js
@@ -0,0 +1,40 @@
+// Star Pattern
+let n=5
+
+for (let i=0;i<n;i++){
+    let row = "";
+    for(let j=0;j<n;j++){
+        row = row + " *"
+    }
+    console.log(row)
+}
+
+// *
+// **
+// ***
+// ****
+
+for(let i=0;i<n;i++){
+    let row = ""
+    for(j=0;j<i+1;j++){
+        row = row + "*"
+    }
+    console.log(row)
+}
+
+
+for (let i=1;i<n;i++){
+    let row = "";
+    for(let j=0;j<i;j++){
+        row =row+(j+1)
+    }
+    console.log(row)
+}
+
+for(let i=0;i<n;i++){
+    let row = ""
+    for(let j=0;j<=i;j++){
+        row=row+(i+1)
+    }
+    console.log(row)
+}
--- loops/secondLargestN.js
+++ loops/secondLargestN.js
@@ -1,20 +1,25 @@
 // Find Second Largest number in an array
-
 function secondLargest(arr){
-    let L = arr[0]
-    let SL = arr[0]
+    if(arr.length<2){
+        return -1
+    }
+    
+    let L = -Infinity
+    let SL = -Infinity
+
     for (let i=0;i<arr.length;i++){
         if(arr[i]>L){
             SL = L;
             L = arr[i]
-        } else if (arr[i] > SL){
+        } else if (arr[i] > SL && arr[i] < L){
             SL = arr[i]
         }
     }
     return SL
 }
 
-let arr = [4,9,0,2,8,7,1]
+let arr = [10,30,20,30]
 
 let result = secondLargest(arr)
+
 console.log(result)
